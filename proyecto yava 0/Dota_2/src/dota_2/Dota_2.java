7984de1475dbad23b1b014f2736807cda675573e
Daniel Romero <ntw.dromero@gmail.com>
2018-06-27 21:41:48 -0500

    prueba subida

---
+++ proyecto yava 0/Dota_2/src/dota_2/Dota_2.java
@@ -0,0 +1,22 @@
+
+package dota_2;
+import java.util.Scanner;
+
+public class Dota_2 {
+
+    public static void main(String[] args) {
+           double b,h,ar;
+           Scanner leer= new Scanner(System.in);
+           System.out.println("ingrese el valor de la base");
+           b=leer.nextDouble();
+           System.out.println("ingrese valor de la altura");
+           h=leer.nextDouble();
+           ar=(b*h)/2;
+           System.out.println("El Area del triangulo es :"+ar);
+           
+           
+    
+    
+    }
+    
+}
